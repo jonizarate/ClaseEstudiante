95ff875678db54a3a4cbce8632773680b7fad4f1
jonizarate <jonizarate00@gmail.com>
2022-10-31 03:31:47 -0300

    Agregar archivos de proyecto.

---
+++ Estudiante.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseEstudiante
+{
+    internal class Estudiante
+    {
+        public string nombre { set; get; }
+        public int numeroDeCuenta { set; get; }
+        public short edad { set; get; }
+        public int dni { set; get; }
+        public string lugarDeNacimiento { set; get; }
+        public string lugarDeProcedencia { set; get; } //nacionalidad
+        public string carreraEstudiantil { set; get; }
+        public bool asistioAClases { set; get; }
+
+        public Estudiante()
+        {
+            nombre = "";
+            numeroDeCuenta = 0;
+            edad = 0;
+            dni = 0;
+            lugarDeNacimiento = "";
+            lugarDeProcedencia = "";
+            carreraEstudiantil = "";
+            asistioAClases = true;
+        }
+        public Estudiante(string nombre, int numeroDeCuenta, short edad, int dni, string lugarDeNacimiento, string lugarDeProcedencia, string carreraEstudiantil,bool asistioAClases)
+        {
+            this.nombre = nombre;
+            this.numeroDeCuenta = numeroDeCuenta;
+            this.edad = edad;
+            this.dni = dni;
+            this.lugarDeNacimiento = lugarDeNacimiento;
+            this.lugarDeProcedencia = lugarDeProcedencia;
+            this.carreraEstudiantil = carreraEstudiantil;
+            this.asistioAClases = asistioAClases;
+        }
+
+        public bool AsistenciaEnClase()
+        {
+            if (asistioAClases == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool EsMayorDeEdad()
+        {
+            return edad >= 18;
+        }
+        public bool NacimientoProcedenciaIgual()
+        {
+            return lugarDeNacimiento.Equals(lugarDeProcedencia);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using ClaseEstudiante;
+
+Estudiante yoestudiante = new Estudiante("esteban",123521,25,44565474,"Buenos Aires","Argentina","Programacion",false);
+
+if (yoestudiante.AsistenciaEnClase())
+{
+    Console.WriteLine("Asistio a clases");
+}
+else
+{
+    Console.WriteLine("No asistio a clases");
+}
+if (yoestudiante.NacimientoProcedenciaIgual())
+{
+    Console.WriteLine("son iguales");
+}
+else
+{
+    Console.WriteLine("no son el mismo lugar");
+}
+if (yoestudiante.EsMayorDeEdad())
+{
+    Console.WriteLine("es mayor de edad");
+}
+else
+{
+    Console.WriteLine("es menor de edad");
+}
